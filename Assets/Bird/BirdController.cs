eedde89ee5c636677633a93e4e2def155993fa1a
Luciano Maiwald <luciano.maiwald@gmail.com>
2014-04-28 15:05:20 +0200

    initial commit

---
+++ Assets/Bird/BirdController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	public float moveSpeed = 3;
+	private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+
+		if (Input.GetButton("Fire1")) {
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+
+			Vector3 target = moveDirection * moveSpeed + currentPosition;
+			transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/CubeMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMover : MonoBehaviour {
+
+	private Camera camera;
+	private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+		position = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 followPosition = Input.mousePosition;
+
+		logPosition("object world", position);
+		logPosition("mouse world", camera.ScreenToWorldPoint(followPosition));
+		logPosition("screen point", followPosition);
+		logPosition("screen object", camera.WorldToScreenPoint(position));
+
+		gameObject.transform.position = new Vector3(
+			camera.ScreenToWorldPoint(followPosition).x,
+			camera.ScreenToWorldPoint(followPosition).y,
+			0);
+	}
+
+	void logPosition(string message, Vector3 p) {
+		Debug.Log (message + " " + p.x + ", " + p.y + ", " + p.z);
+	}
+}
