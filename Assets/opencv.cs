a0bb5db7e4b796b8613a8713ac46996428b1ffd9
Luciano Maiwald <luciano.maiwald@gmail.com>
2014-05-05 13:01:22 +0200

    add new bird

---
+++ Assets/opencv.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+
+
+public class opencv : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CvMat src = new CvMat("lenna.png", LoadMode.GrayScale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
